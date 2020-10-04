3eb46bbb30d35532f358b6044c50a870b7f224b6
Lokanadham Osuri <losuri@GLIC.COM>
2020-10-04 21:30:39 +0530

    Updates

---
+++ precache-manifest.6638f2fa323c4354f272bd0f525f8bd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81cf8f360a66d7196960aea0694f5a9d",
+    "url": "/myfirst-basic-react-app/index.html"
+  },
+  {
+    "revision": "d1e9dd6e5736d23ef070",
+    "url": "/myfirst-basic-react-app/static/css/main.ad29733d.chunk.css"
+  },
+  {
+    "revision": "e05509d8c24914c5433e",
+    "url": "/myfirst-basic-react-app/static/js/2.1999375b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myfirst-basic-react-app/static/js/2.1999375b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1e9dd6e5736d23ef070",
+    "url": "/myfirst-basic-react-app/static/js/main.4d9b4b57.chunk.js"
+  },
+  {
+    "revision": "132307c60c5be2b6e4c8",
+    "url": "/myfirst-basic-react-app/static/js/runtime-main.874b272c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/osuri/myfirst-basic-react-app/precache-manifest.9e4445cc50121d4124641f435febb877.js"
+  "/myfirst-basic-react-app/precache-manifest.6638f2fa323c4354f272bd0f525f8bd5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/osuri/myfirst-basic-react-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myfirst-basic-react-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
