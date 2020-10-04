7dfbcb82161ec7a66043f899680bfef5e7bea1a1
Lokanadham Osuri <losuri@GLIC.COM>
2020-10-04 21:23:20 +0530

    Updates

---
+++ precache-manifest.9e4445cc50121d4124641f435febb877.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70e934739a47bb968cd06ff57900b185",
+    "url": "/osuri/myfirst-basic-react-app/index.html"
+  },
+  {
+    "revision": "d1e9dd6e5736d23ef070",
+    "url": "/osuri/myfirst-basic-react-app/static/css/main.ad29733d.chunk.css"
+  },
+  {
+    "revision": "e05509d8c24914c5433e",
+    "url": "/osuri/myfirst-basic-react-app/static/js/2.1999375b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/osuri/myfirst-basic-react-app/static/js/2.1999375b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1e9dd6e5736d23ef070",
+    "url": "/osuri/myfirst-basic-react-app/static/js/main.4d9b4b57.chunk.js"
+  },
+  {
+    "revision": "e1f65a92d0bf86cb1764",
+    "url": "/osuri/myfirst-basic-react-app/static/js/runtime-main.b0f09a5f.js"
+  }
+]);
